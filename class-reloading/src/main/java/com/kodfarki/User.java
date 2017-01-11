d15900b1e6cc401b4624abf6ed4746377d662c7a
ekarhal <halil.karakose@ericsson.com>
2017-01-11 17:00:00 +0300

    removed old project

--- class-reloading/src/main/java/com/kodfarki/User.java
+++
@@ -1,9 +0,0 @@
-package com.kodfarki;
-
-/**
- * User: Halil Karakose
- * Date: 15/07/16
- * Time: 16:38
- */
-public class User {
-}
--- class-reloading/src/main/java/qj/blog/classreloading/example1/StaticInt.java
+++
@@ -1,9 +0,0 @@
-package PACKAGE_NAME;
-
-/**
- * User: Halil Karakose
- * Date: 15/07/16
- * Time: 16:39
- */
-public class StaticInt {
-}
